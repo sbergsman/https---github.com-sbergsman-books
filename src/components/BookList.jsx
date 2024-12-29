d6d429c137da1fa714c2266e685cc37c05b4d962
steve <steven.bergsman@gmail.com>
2024-12-28 21:44:20 -0600

    120: basic book app checkin

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,6 +1,13 @@
-function BookList() {
-    return <div>
-        BookList
+import BookShow from "./BookShow";
+
+function BookList({ books, onDelete, onEdit }) {
+
+    const renderedBooks = books.map((book) => {
+        return <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />;
+    });
+
+    return <div className="book-list">
+        {renderedBooks}
     </div>
 };
 
